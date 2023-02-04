2ec3d5e48fc8a3216ae60b44156f8b6416164b3c
Sergei Podmarkov <tweezy050794@gmail.com>
2023-02-04 11:25:41 +0300

    add timer

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
-let textGame = document.getElementById('text2');
+let textGame = document.getElementById("text2");
 
-let clickButton = document.querySelector('.button');
+let clickButton = document.querySelector(".button");
 
-
-let timesLeft = document.querySelector('.times-left');
-
-
+let timesLeft = document.querySelector(".times-left");
 
 let counter = 0;
 
-clickButton.addEventListener('click', () => {
-    
-    if (counter === 0) {
-        setTimeout(timeIsOut, 5000);
-    }
+let timer;
 
-    counter++
-
-    textGame.textContent = `${counter}!`;
-
-});
-  
-
-
-function timeIsOut() {
-    alert(`TIME IS END! YOUR SCORE = ${counter}! \nNICE ONE!`);
-
-        counter = 0;
-        
-        textGame.textContent = `${counter}!`; 
+function initTimer(maxTime) {
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timesLeft.textContent = `У Вас Осталось ${maxTime} Секунд`);
+    } else if (maxTime === 0) {
+      clearInterval(timer);
+      timesLeft.textContent = `У Вас Осталось ${maxTime} Секунд`;
+    };
+  }, 1000);
 };
 
+clickButton.addEventListener("click", () => {
+  if (counter === 0) {
+    initTimer(5);
+    setTimeout(timeIsOut, 5000);
+  };
+
+  counter++;
+
+  textGame.textContent = `${counter}!`;
+});
+
+function timeIsOut() {
+  alert(`TIME IS END! YOUR SCORE = ${counter}!
+NICE ONE!`);
+
+  counter = 0;
+
+  textGame.textContent = `${counter}!`;
+};
